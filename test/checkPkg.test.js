ffea06e700892e34fe4239de9f9ef006aa41e8cb
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-10-08 17:53:10 +0800

    Update tests

--- test/checkPkg.test.js
+++ test/checkPkg.test.js
@@ -1,27 +1,13 @@
 import checkPkg from "../src/checkPkg";
 
-describe('checkPkg', () => {
-  it('should package jest be found', done => {
-    const onFound = jest.fn().mockImplementation(callback);
-    const onNotFound = jest.fn().mockImplementation(callback);
-
-    function callback() {
-      expect(onFound).toBeCalled();
-      expect(onNotFound).not.toBeCalled();
-      done();
-    };
-    checkPkg('jest', {}, onFound, onNotFound);
+describe("checkPkg", () => {
+  it("should package jest be found", async () => {
+    const hasFound = await checkPkg("jest", {});
+    expect(hasFound).toBe(true);
   });
 
-  it('should package antd not be found', done => {
-    const onFound = jest.fn().mockImplementation(callback);
-    const onNotFound = jest.fn().mockImplementation(callback);
-
-    function callback() {
-      expect(onFound).not.toBeCalled();
-      expect(onNotFound).toBeCalled();
-      done();
-    }
-    checkPkg('antd', {}, onFound, onNotFound);
+  it("should package antd not be found", async () => {
+    const hasFound = await checkPkg("antd", {});
+    expect(hasFound).toBe(false);
   });
 });
